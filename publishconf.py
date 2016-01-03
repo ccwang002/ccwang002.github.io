f08cd0f722652020b9ac884bf2413fac04221f3a
Liang Bo Wang <ccwang002@gmail.com>
2016-01-03 21:08:03 +0800

    drop protocol in SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://blog.liang2.tw'
+SITEURL = '//blog.liang2.tw'
 SITELOGO = SITEURL + SITELOGO
 RELATIVE_URLS = False
 
