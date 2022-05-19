046ef5d6c3b1aa5d8868258352c1a45f9a70e86e
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2022-05-19 23:10:42 +0800

    Generate Pelican site

--- theme/scripts/theme-toggle.js
+++ theme/scripts/theme-toggle.js
@@ -7,15 +7,29 @@ if (currentTheme == "dark"
     toggleTheme();
 }
 
+function updateUtterancesTheme(theme) {
+    const iframe = document.querySelector('.utterances-frame');
+    if (iframe) {
+        const message = {
+            type: 'set-theme',
+            theme: theme
+        };
+        iframe.contentWindow.postMessage(message, 'https://utteranc.es');
+    }
+}
+
 function toggleTheme() {
     if (document.body.classList.contains("dark-theme")) {
         document.body.classList.remove("dark-theme");
         themeToggleBtn.innerText = "🌙";
+        updateUtterancesTheme("github-light");
         localStorage.setItem("theme", "light");
     } else {
         document.body.classList.add("dark-theme");
         themeToggleBtn.innerText = "☀️";
+        updateUtterancesTheme("github-dark");
         localStorage.setItem("theme", "dark");
     }
 }
+
 themeToggleBtn.addEventListener("click", toggleTheme);
