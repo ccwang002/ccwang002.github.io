4f6f125fe9f76dc451207f83340191c29682ba18
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2022-05-19 22:43:01 +0800

    Generate Pelican site

---
+++ theme/scripts/theme-toggle.js
@@ -0,0 +1,21 @@
+const themeToggleBtn = document.querySelector(".theme-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark"
+    || (currentTheme === undefined && prefersDarkScheme.matches)) {
+    toggleTheme();
+}
+
+function toggleTheme() {
+    if (document.body.classList.contains("dark-theme")) {
+        document.body.classList.remove("dark-theme");
+        themeToggleBtn.innerText = "🌙";
+        localStorage.setItem("theme", "light");
+    } else {
+        document.body.classList.add("dark-theme");
+        themeToggleBtn.innerText = "☀️";
+        localStorage.setItem("theme", "dark");
+    }
+}
+themeToggleBtn.addEventListener("click", toggleTheme);
